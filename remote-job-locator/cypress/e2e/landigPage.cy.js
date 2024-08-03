1fe56b1cd62f945407241e0392ce5533b416bf8a
jordan williamson <williamsonjordan05@gmail.com>
2024-08-03 10:26:52 -0600

    completed testing suites with everything passing as expected

--- remote-job-locator/cypress/e2e/landigPage.cy.js
+++ remote-job-locator/cypress/e2e/landigPage.cy.js
@@ -1,5 +1,21 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Landing page tests', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://remotive.com/api/remote-jobs', {
+      statusCode: 200,
+      fixture: 'example.json'
+    }).as('jobApi');
+    cy.visit('http://localhost:3000/')
   })
-})
+  it('Should cotain the main page content', () => {
+    cy.get('.landingPageWrapper .mainContent').should('exist')
+    .and('contain', 'Looking for your next or first remote job?')
+    .and('contain', 'Click below to get started!');
+  })
+  it('Should contain a button', () => {
+    cy.get('.landingPageWrapper .mainContent .postingsButton').should('exist')
+  })
+  it('Should have a clickable button that takes you to the postings', () => {
+    cy.get('.landingPageWrapper .mainContent .postingsButton').click();
+    cy.url().should('contain', '/postings')
+  })
+});
--- remote-job-locator/cypress/e2e/savedPostings.cy.js
+++ remote-job-locator/cypress/e2e/savedPostings.cy.js
@@ -1,5 +1,27 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Saved Postings Tests', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://remotive.com/api/remote-jobs', {
+      statusCode: 200,
+      fixture: 'example.json'
+    }).as('jobApi');
+    cy.visit('http://localhost:3000/SavedPostings')
   })
+  it('Should show a main header on page load', () => {
+    cy.get('.savedWrapper .mainHeader').should('exist');
+    cy.get('.savedWrapper .mainHeader').should('contain', 'Saved Postings')
+  });
+  it('Saves a posting and views it in the saved postings page', () => {
+    cy.visit('http://localhost:3000/postings');
+    cy.get(':nth-child(3) > .contentWrapper > .secondContent > .saveForLater').click();
+    cy.wait(5000); 
+    
+    cy.visit('http://localhost:3000/SavedPostings');
+    
+    cy.get('.savedWrapper').should('contain', 'Senior Healthcare Data Analyst') 
+    .and('contain', 'Position: Senior Healthcare Data Analyst')
+    .and('contain', 'Company: Imagen Technologies')
+    .and('contain', '$90,000 - $120,000 plus benefits')
+    .and('contain', 'Posted: Jul 27 2024')
+    .and('contain', 'Candidate Location: USA')
+  });
 })
